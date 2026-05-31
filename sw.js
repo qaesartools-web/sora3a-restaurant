cfa5fd770300bef994267e0de2cc9aca5233bb9c
qaesartools-web <qaesartools@gmail.com>
2026-06-01 01:58:19 +0300

    Add cache-busting SW

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
+const CACHE_NAME = 'rest2-v1780268298';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+});
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
     .then(() => self.clients.claim())
   );
 });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request, {cache: 'no-store'}));
+});
