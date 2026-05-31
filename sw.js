cec04bf0efd92ef215f35278b096b5a6c4d77a94
qaesartools-web <qaesartools@gmail.com>
2026-06-01 01:34:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
